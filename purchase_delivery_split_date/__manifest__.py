35749348171d661867605b0b8f9a26628ea16ddc
Dominik Schmidt <dominik.schmidt@ametras.com>
2023-08-07 16:39:39 +0200

    [FIX] purchase_delivery_split_date: avoid KeyError during mass import

--- purchase_delivery_split_date/__manifest__.py
+++ purchase_delivery_split_date/__manifest__.py
@@ -7,7 +7,7 @@
     "version": "13.0.1.0.7",
     "summary": "Allows Purchase Order you confirm to generate one Incoming "
     "Shipment for each expected date indicated in the Purchase Order Lines",
-    "author": "Numerigraphe, ForgeFlow, Camptocamp, Odoo Community Association (OCA), Dominik Schmidt",
+    "author": "Numerigraphe, ForgeFlow, Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Purchase Management",
     "license": "AGPL-3",
